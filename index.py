e74b3b0fcc29cb866f43c55e2a7919668e11c644
Lin-NZ <106861101+Lin-NZ@users.noreply.github.com>
2023-09-21 16:56:39 +0800

    Update index.py

--- index.py
+++ index.py
@@ -39,7 +39,7 @@ if media_file is not None:  # Check if a file has been uploaded
         summary_response = openai.ChatCompletion.create(
             model = 'gpt-3.5-turbo',
             messages = [
-                {"role": "user", "content": input("你是個得力的文書處理助手。")}
+                {"role": "user", "content": input("你是個得力的文書處理助手。")},
                 {"role": "assistant", "content": "我是一個基於人工智慧的語言模型，設計來幫助處理各種文書處理任務。如果您有任何需要，不論是文字處理、文件編輯、資訊檢索或其他任何事情，請隨時告訴我，我會盡力提供幫助。請問您有什麼特定的問題或工作，我可以協助您處理嗎？"},
                 {"role": "user", "content": input("幫我把會議紀錄根據內容整理成段落，再根據段落整理成表格，表格內容須顯示段落主題、約50字摘要，並在表個最後一列顯示最終結論。")},
                 {"role": "assistant", "content": "當然可以幫您進行這項任務。請提供會議紀錄的內容，我將根據您的要求整理成表格，包括段落主題、約50字的摘要，以及最終結論。請提供會議紀錄的文本或內容，我將嘗試協助您處理。"},
