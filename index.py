d2d7507ebb23389fe05f356b71b21051e3b523ee
Nicholas <106821355+MortaXN@users.noreply.github.com>
2023-09-25 22:00:02 +0800

    Update index.py

--- index.py
+++ index.py
@@ -60,9 +60,49 @@ def summarize_audio(tr_response):
         )
         return summary_response
 
+def record_audio(filename, duration):
+    p = pyaudio.PyAudio()
+
+    # Configure the audio stream
+    stream = p.open(format=pyaudio.paInt16,
+                    channels=1,
+                    rate=44100,
+                    input=True,
+                    frames_per_buffer=1024)
+
+    st.text("Recording...")
+
+    frames = []
+
+    for _ in range(0, int(44100 / 1024 * duration)):
+        data = stream.read(1024)
+        frames.append(data)
+
+    st.text("Recording Finished!")
+
+    # Stop and close the audio stream
+    stream.stop_stream()
+    stream.close()
+
+    # Save the recorded audio to a WAV file
+    wf = wave.open(filename, 'wb')
+    wf.setnchannels(1)
+    wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
+    wf.setframerate(44100)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
 # Record Page
 if selected == "Record":
     st.title('Record')
+    
+    duration = st.slider("Recording Duration (seconds)", 1, 10, 5)
+
+    if st.button("Start Recording"):
+        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        filename = f"recorded_audio_{timestamp}.wav"
+        record_audio(filename, duration)
+        st.audio(filename, format = "audio/wav")
 
 # Upload Page
 if selected == "Upload":
