070d896603026b283d83ab0c6e615d58618c62d3
Nicholas <106821355+MortaXN@users.noreply.github.com>
2023-12-01 23:51:21 +0800

    Update index.py

--- index.py
+++ index.py
@@ -117,18 +117,24 @@ if selected == "Q&A":
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
     
-    if prompt := st.chat_input("What is up?"):
+    if prompt := st.chat_input("What do you want to know?"):
         st.session_state.messages.append({"role": "user", "content": prompt})
+        
         with st.chat_message("user"):
             st.markdown(prompt)
+            
         with st.chat_message("assistant"):
             message_placeholder = st.empty()
             full_response = ""
+            
             for response in openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[{"role": m["role"], "content": m["content"]}
+                model = "gpt-3.5-turbo",
+                messages = [{"role": m["role"], "content": m["content"]}
                           for m in st.session_state.messages], stream=True):
+                              
                 full_response += response.choices[0].delta.get("content", "")
                 message_placeholder.markdown(full_response + "▌")
+                              
             message_placeholder.markdown(full_response)
+            
         st.session_state.messages.append({"role": "assistant", "content": full_response})
